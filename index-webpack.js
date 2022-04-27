707fc2eedfd8afe2a118a9765832836a24e9ea39
Bastian Schwabe <bas@neuedaten.de>
2022-04-27 17:57:45 +0200

    fix jquery problem

--- index-webpack.js
+++ index-webpack.js
@@ -1,6 +1,8 @@
 
 if (typeof jQuery === 'undefined') throw new Error('featherlight requires a window jQuery, for now');
 
+$ = jQuery;
+
 require('./src/featherlight.css');
 require('./src/featherlight');
 
